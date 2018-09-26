3ca43dde5f7b422652d1473659476bb0e077a822
hx-lance <lance2038@hotmail.com>
2018-09-26 12:10:49 +0800

    111111

--- getpic/README.md
+++
@@ -1,4 +0,0 @@
-密钥等配置在
-com.ocr.Constant
-执行识别在
-com.x_execute下
--- getpic/src/main/java/com/x_execute/useBaiduApi.java
+++
@@ -1,9 +0,0 @@
-package com.x_execute;
-
-public class useBaiduApi
-{
-    public static void main(String[] args) throws Exception
-    {
-
-    }
-}
